5a830136dadcbc11105750a5b6a2b967dbcd8337
DoaaDalaq <102347716+DoaaDalaq@users.noreply.github.com>
2022-05-23 04:22:15 +0400

    Update Contact-us.php

--- Contact-us.php
+++ Contact-us.php
@@ -14,7 +14,7 @@
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="css/cover.css" rel="stylesheet">
 
-    <!--Fontawesome CDN 1234-->
+    <!--Fontawesome CDN-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
         integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/Sign-in.css">
